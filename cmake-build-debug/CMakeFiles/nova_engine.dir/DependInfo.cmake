dc40ea414bbf455dc93b1b60c82822bc4aed4a63
abkarada <abdurrahmankaradag19@gmail.com>
2025-08-13 13:36:04 +0300

    One for send-receive & other one is calculation

--- cmake-build-debug/CMakeFiles/nova_engine.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/nova_engine.dir/DependInfo.cmake
@@ -12,12 +12,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/ryuzaki/Desktop/media_engine_test_2/src/nova_engine.cpp" "CMakeFiles/nova_engine.dir/src/nova_engine.cpp.o" "gcc" "CMakeFiles/nova_engine.dir/src/nova_engine.cpp.o.d"
   )
 
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
-  )
-
-# Targets to which this target links which contain Fortran sources.
-set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
